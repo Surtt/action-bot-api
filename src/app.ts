bcd3b038f67827227d97164626fb6f1f61d1bbb1
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-11-27 21:10:41 +0200

    add exception filter

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { Server } from 'http';
 import { LoggerService } from './logger/logger.service';
 import { ILogger } from './logger/logger.interface';
 import { UsersController } from './users/users.controller';
+import { ExceptionFilter } from './errors/exception.filter';
 
 export class App {
 	app: Express;
@@ -10,21 +11,32 @@ export class App {
 	port: number;
 	logger: ILogger;
 	userController: UsersController;
+	exceptionFilter: ExceptionFilter;
 
-	constructor(logger: LoggerService, userController: UsersController) {
+	constructor(
+		logger: LoggerService,
+		userController: UsersController,
+		exceptionFilter: ExceptionFilter,
+	) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
 		this.userController = userController;
+		this.exceptionFilter = exceptionFilter;
 	}
 
 	useRoutes(): void {
 		this.app.use('/users/', this.userController.router);
 	}
 
+	useExceptionFilter() {
+		this.app.use(this.exceptionFilter.catch);
+	}
+
 	public async init(): Promise<void> {
-		this.server = this.app.listen(this.port);
 		this.useRoutes();
+		this.useExceptionFilter();
+		this.server = this.app.listen(this.port);
 		this.logger.log(`Server: http://localhost:${this.port}`);
 	}
 }
