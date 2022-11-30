47ec6840fc1becf2b6d443fa89d0d28072955414
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-11-30 22:29:32 +0200

    add prisma service

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { Symbols } from './symbols';
 import 'reflect-metadata';
 import { IConfigService } from './config/config.service.interface';
 import { IExceptionFilter } from './errors/exception.filter.interface';
+import { PrismaService } from './database/prisma.service';
 
 @injectable()
 export class App {
@@ -19,6 +20,7 @@ export class App {
 		@inject(Symbols.UserController) private userController: UsersController,
 		@inject(Symbols.ExceptionFilter) private exceptionFilter: IExceptionFilter,
 		@inject(Symbols.ConfigService) private configService: IConfigService,
+		@inject(Symbols.PrismaService) private prismaService: PrismaService,
 	) {
 		this.app = express();
 		this.port = 8000;
@@ -40,6 +42,7 @@ export class App {
 		this.useMiddleware();
 		this.useRoutes();
 		this.useExceptionFilter();
+		await this.prismaService.connect();
 		this.server = this.app.listen(this.port);
 		this.logger.log(`Server: http://localhost:${this.port}`);
 	}
--- src/main.ts
+++ src/main.ts
@@ -12,12 +12,14 @@ import { IUsersService } from './users/users.service.interface';
 import { UsersService } from './users/users.service';
 import { IConfigService } from './config/config.service.interface';
 import { ConfigService } from './config/config.service';
+import { PrismaService } from './database/prisma.service';
 
 const appBindings = new ContainerModule((bind: Bind) => {
 	bind<ILogger>(Symbols.ILogger).to(LoggerService).inSingletonScope();
 	bind<IExceptionFilter>(Symbols.ExceptionFilter).to(ExceptionFilter);
 	bind<IUserController>(Symbols.UserController).to(UsersController);
 	bind<IUsersService>(Symbols.UserService).to(UsersService);
+	bind<PrismaService>(Symbols.PrismaService).to(PrismaService).inSingletonScope();
 	bind<IConfigService>(Symbols.ConfigService).to(ConfigService).inSingletonScope();
 	bind<App>(Symbols.Application).to(App);
 });
