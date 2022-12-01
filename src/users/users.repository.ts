5dc2dc5d7f94210d569edfd73111f3e809f5c1ed
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-12-01 22:05:38 +0200

    add users repository (#8)

---
+++ src/users/users.repository.ts
@@ -0,0 +1,28 @@
+import { IUsersRepository } from './users.repository.interface';
+import { User } from './user.entity';
+import { UserModel } from '@prisma/client';
+import { inject, injectable } from 'inversify';
+import { Symbols } from '../symbols';
+import { PrismaService } from '../database/prisma.service';
+
+@injectable()
+export class UsersRepository implements IUsersRepository {
+	constructor(@inject(Symbols.PrismaService) private prismaService: PrismaService) {}
+	async create({ name, email, password }: User): Promise<UserModel> {
+		return this.prismaService.client.userModel.create({
+			data: {
+				name,
+				email,
+				password,
+			},
+		});
+	}
+
+	async find(email: string): Promise<UserModel | null> {
+		return this.prismaService.client.userModel.findFirst({
+			where: {
+				email,
+			},
+		});
+	}
+}
